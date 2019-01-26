8ca8307c47d89a0c47a4f3431baf37e15eb660f4
Evan Cole <18554853+colevandersWands@users.noreply.github.com>
2019-01-26 15:46:16 +0100

    Create read.js

---
+++ read.js
@@ -0,0 +1,18 @@
+function read(file_name) {
+  console.clear();
+
+  var head = document.getElementsByTagName('head')[0];
+  while(head.firstChild){
+    head.removeChild(head.firstChild);
+  }
+
+  var js = document.createElement("script");
+
+  js.type = "text/javascript";
+
+  js.src = file_name;
+
+  head.appendChild(js);
+
+  return 'loaded script ' + file_name;
+}
